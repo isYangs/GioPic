53f96abecc17d008561c2336d74fdd4ff05954e2
isYangs <isYangs@foxmail.com>
2024-05-01 04:04:40 +0800

    feat(api): ✨ 新增了兰空社区版获取策略的方法(未测试)

--- src/api/index.ts
+++ src/api/index.ts
@@ -27,7 +27,18 @@ function createRequest(url: string, token?: string, customHeaders?: Record<strin
 
 const requestData = {
   /**
-   * 兰空获取策略
+   * 兰空社区版获取策略
+   *
+   * @param url - 图床API地址，例如传入appStore中的apiUrl。
+   * @param token - 用于身份验证的 token，例如传入appStore中的token。
+   * @returns 返回一个 Promise，当策略获取成功时，Promise 将被解析。
+   */
+  getLskyStrategies: (url: string, token: string) => {
+    const req = createRequest(url, token)
+    return req('/api/v1/strategies', 'get')
+  },
+  /**
+   * 兰空企业版获取策略
    *
    * @param url - 图床API地址，例如传入appStore中的apiUrl。
    * @param token - 用于身份验证的 token，例如传入appStore中的token。
@@ -39,14 +50,14 @@ const requestData = {
   },
 
   /**
-   * 兰空上传图片
+   * 兰空企业版上传图片
    *
    * @param url - 图床API地址，例如传入appStore中的apiUrl。
    * @param token - 用于身份验证的 token，例如传入appStore中的token。
    * @param data - 接收一个对象，对象中传入上传的图片，权限，策略id，相册id，过期时间。
    * @returns 返回一个Promise对象，表示异步上传操作
    */
-  uploadLskyImage: (url: string, token: string, data: UploadData) => {
+  uploadLskyProImage: (url: string, token: string, data: UploadData) => {
     const req = createRequest(url, token, {
       'Content-Type': 'multipart/form-data',
     })
