f4d2f02cafe258c3784375b26f55e7e0b38022a1
isYangs <isYangs@foxmail.com>
2024-05-03 11:18:36 +0800

    build(unocss): 🔧 移除配置文件中多余的快捷方式

--- uno.config.ts
+++ uno.config.ts
@@ -21,8 +21,6 @@ export default defineConfig({
     'wh-full': 'w-full h-full',
     'text-overflow': 'text-ellipsis overflow-hidden whitespace-nowrap',
     'flex-center': 'flex items-center justify-center',
-    'widgets-btn': 'w40px h40px flex-center bg-[#20202a] dark:(bg-[#f0f0f0] text-black) rounded-6px text-white select-none',
-
   },
   rules: [['object-center', { 'object-position': 'center' }]],
   transformers: [
