b37a5b2009adb52ab6771f41fef813cc8a629b46
isYangs <isYangs@foxmail.com>
2024-08-10 13:23:15 +0800

    fix: 🐞 修复无法兼容win系统的问题

--- vite.config.ts
+++ vite.config.ts
@@ -67,7 +67,6 @@ export default defineConfig({
   ],
 })
 
-
 // https://github.com/WiseLibs/better-sqlite3/blob/v8.5.2/lib/database.js#L36
 // https://github.com/WiseLibs/better-sqlite3/blob/v8.5.2/lib/database.js#L50
 function bindingSqlite3(options: {
@@ -84,12 +83,13 @@ function bindingSqlite3(options: {
     name: 'vite-plugin-binding-sqlite3',
     config(config) {
       // https://github.com/vitejs/vite/blob/v4.4.9/packages/vite/src/node/config.ts#L496-L499
-      const resolvedRoot = normalizePath(config.root ? path.resolve(config.root) : process.cwd())
-      const output = path.posix.resolve(resolvedRoot, options.output)
+      const path$1 = process.platform === 'win32' ? path.win32 : path.posix
+      const resolvedRoot = config.root ? path$1.resolve(config.root) : process.cwd()
+      const output = path$1.resolve(resolvedRoot, options.output)
       const better_sqlite3 = require.resolve('better-sqlite3')
-      const better_sqlite3_root = path.posix.join(better_sqlite3.slice(0, better_sqlite3.lastIndexOf('node_modules')), 'node_modules/better-sqlite3')
-      const better_sqlite3_node = path.posix.join(better_sqlite3_root, 'build/Release', options.better_sqlite3_node)
-      const better_sqlite3_copy = path.posix.join(output, options.better_sqlite3_node)
+      const better_sqlite3_root = path$1.join(better_sqlite3.slice(0, better_sqlite3.lastIndexOf('node_modules')), 'node_modules/better-sqlite3')
+      const better_sqlite3_node = path$1.join(better_sqlite3_root, 'build/Release', options.better_sqlite3_node)
+      const better_sqlite3_copy = path$1.join(output, options.better_sqlite3_node)
       if (!fs.existsSync(better_sqlite3_node))
         throw new Error(`${TAG} Can not found "${better_sqlite3_node}".`)
 
@@ -98,7 +98,7 @@ function bindingSqlite3(options: {
 
       fs.copyFileSync(better_sqlite3_node, better_sqlite3_copy)
       /** `dist-native/better_sqlite3.node` */
-      const BETTER_SQLITE3_BINDING = better_sqlite3_copy.replace(`${resolvedRoot}/`, '')
+      const BETTER_SQLITE3_BINDING = better_sqlite3_copy.replace(resolvedRoot + path.sep, '')
       fs.writeFileSync(path.join(resolvedRoot, '.env'), `VITE_BETTER_SQLITE3_BINDING=${BETTER_SQLITE3_BINDING}`)
 
       console.log(TAG, `binding to ${BETTER_SQLITE3_BINDING}`)
