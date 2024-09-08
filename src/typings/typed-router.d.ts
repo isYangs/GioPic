d5e614770a903123ca36ba28b1c5ebb14751bc39
isYangs <isYangs@foxmail.com>
2024-09-09 01:08:29 +0800

    fix: 🐞 修复新页面路由类型且移除多余的导入

--- src/typings/typed-router.d.ts
+++ src/typings/typed-router.d.ts
@@ -40,6 +40,7 @@ import type {
 declare module 'vue-router/auto/routes' {
   export interface RouteNamedMap {
     'Home': RouteRecordInfo<'Home', '/', Record<never, never>, Record<never, never>>,
+    '/About': RouteRecordInfo<'/About', '/About', Record<never, never>, Record<never, never>>,
     '/Images/': RouteRecordInfo<'/Images/', '/Images', Record<never, never>, Record<never, never>>,
     '/Setting/': RouteRecordInfo<'/Setting/', '/Setting', Record<never, never>, Record<never, never>>,
     '/Setting/[id]': RouteRecordInfo<'/Setting/[id]', '/Setting/:id', { id: ParamValue<true> }, { id: ParamValue<false> }>,
