1fbb46b8e2cff6f53a5bf053f29b89f70fc9c1f6
isYangs <isYangs@foxmail.com>
2024-09-13 02:50:54 +0800

    fix: 🐞 windows rebuild error

--- rebuild.js
+++ rebuild.js
@@ -2,32 +2,31 @@ import child from 'node:child_process'
 import { createRequire } from 'node:module'
 import path from 'node:path'
 
-// If you prefer electron-rebuild:
-// 👉 https://github.com/WiseLibs/better-sqlite3/blob/v8.5.2/docs/troubleshooting.md#electron
-// 👉 https://stackoverflow.com/questions/46384591/node-was-compiled-against-a-different-node-js-version-using-node-module-versio/52796884#52796884
-
 const require = createRequire(import.meta.url)
 
 const better_sqlite3 = require.resolve('better-sqlite3')
 const better_sqlite3_root = path.posix.join(better_sqlite3.slice(0, better_sqlite3.lastIndexOf('node_modules')), 'node_modules/better-sqlite3')
+
 const cp = child.spawn(
   process.platform === 'win32' ? 'npm.cmd' : 'npm',
   [
     'run',
     'build-release',
     `--target=${process.versions.electron}`,
-    // https://github.com/electron/electron/blob/v26.1.0/docs/tutorial/using-native-node-modules.md#manually-building-for-electron
     '--dist-url=https://electronjs.org/headers',
   ],
   {
     cwd: better_sqlite3_root,
     stdio: 'inherit',
+    shell: true,
   },
 )
 
 cp.on('exit', (code) => {
-  if (code === 0)
+  if (code === 0) {
     console.log('Rebuild better-sqlite3 success.')
-
+  } else {
+    console.error('Failed to rebuild better-sqlite3. Exit code:', code)
+  }
   process.exit(code)
 })
