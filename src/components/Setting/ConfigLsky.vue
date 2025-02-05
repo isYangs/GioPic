9cdeaa87ad306235c59c44c605c8f0d86e49ad41
L33Z22L11 <L33Z22L11@gmail.com>
2025-02-05 14:06:07 +0800

    refactor(Setting): extract SetItem componemt to reduce code

--- src/components/Setting/ConfigLsky.vue
+++ src/components/Setting/ConfigLsky.vue
@@ -40,57 +40,21 @@ async function syncStrategies() {
 
 <template>
   <n-form ref="setFormRef" :rules="rules">
-    <n-card class="set-item mb3 wh-full rounded-2">
-      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
-        <div class="flex items-center">
-          API 地址
-        </div>
-        <n-text class="text-xs op80">
-          http(s)://域名，不含尾随斜杠
-        </n-text>
-      </div>
+    <setting-item title="API 地址" desc="http(s)://域名，不含尾随斜杠">
       <CodeInput v-model:value="setting.api" type="text" placeholder="请填写图床API地址" />
-    </n-card>
-    <n-card class="set-item mb3 wh-full rounded-2">
-      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
-        <div class="flex items-center">
-          Token
-        </div>
-        <n-text class="text-xs op80">
-          例如：1|1bJbwlqBfnggmOMEZqXT5XusaIwqiZjCDs7r1Ob5
-        </n-text>
-      </div>
+    </setting-item>
+
+    <setting-item title="Token" desc="例如：1|1bJbwlqBfnggmOMEZqXT5XusaIwqiZjCDs7r1Ob5">
       <CodeInput v-model:value="setting.token" type="text" placeholder="请填写图床生成的Token" />
-    </n-card>
-    <n-card class="set-item mb3 wh-full rounded-2">
-      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
-        <div class="flex items-center">
-          存储策略
-        </div>
-      </div>
+    </setting-item>
+
+    <setting-item title="存储策略">
       <div class="flex gap-1">
         <n-select v-model:value="activeStrategy" :options="setting.strategies" />
         <n-button @click="syncStrategies">
           <div i-ic-round-refresh />
         </n-button>
       </div>
-    </n-card>
+    </setting-item>
   </n-form>
 </template>
-
-<style scoped>
-.set-item :deep(.n-card__content) {
-  --uno: flex flex-row items-center justify-between;
-}
-
-.set-item :deep(.n-select) {
-  --uno: w50;
-}
-
-.set-item :deep(.n-input) {
-  --uno: w75;
-}
-:deep(.n-form-item) > .n-form-item-feedback-wrapper {
-  --uno: text-xs;
-}
-</style>
