7c3f8644d7a3b2e5122e8f980ca308011ab3e958
isYangs <isYangs@foxmail.com>
2025-02-05 13:28:15 +0800

    feat(Setting): refactor storage settings UI

--- src/components/Setting/ConfigLsky.vue
+++ src/components/Setting/ConfigLsky.vue
@@ -1,94 +1,34 @@
 <script setup lang="ts">
 import type { FormRules } from 'naive-ui'
-import { NButton, NSelect } from 'naive-ui'
 import requestUtils from '~/api'
 import CodeInput from '~/components/Common/CodeInput.vue'
 import type { ProgramDetail } from '~/stores'
 import { useProgramStore } from '~/stores'
-import { renderIcon } from '~/utils/main'
-import { createFormRule, validateLskyToken, validateUrl } from '~/utils/validate'
 
 const route = useRoute('/Setting/[id]')
 const id = ref(Number.parseInt(route.params.id))
 const programStore = useProgramStore()
-const api = ref('')
-const token = ref('')
 const activeStrategy = ref<number | null>(null)
 
-const settings = computed(() => programStore.getProgram(id.value).detail as ProgramDetail['lsky'])
+const setting = computed({
+  get: () => programStore.getProgram(id.value).detail as ProgramDetail['lsky'],
+  set: val => programStore.setProgramDetail(id.value, val),
+})
 
-const setItem = useTemplateRef('setItemRef')
+const setItem = useTemplateRef('setFormRef')
 
 const rules: FormRules = {
-  apiUrl: createFormRule(() => validateUrl(api.value)),
-  token: createFormRule(() => validateLskyToken(token.value)),
+  // apiUrl: createFormRule(() => validateUrl(api.value)),
+  // token: createFormRule(() => validateLskyToken(token.value)),
 }
 
-const settingOptions = computed(() => [
-  {
-    name: 'API 地址',
-    tip: 'http(s)://域名，不含尾随斜杠',
-    width: 300,
-    path: 'apiUrl',
-    component: () => {
-      return h(CodeInput, {
-        value: api.value,
-        type: 'url',
-        placeholder: '请填写图床API地址',
-        onUpdateValue: (val: string) => {
-          api.value = val
-        },
-        onChange: formValidation,
-      })
-    },
-  },
-  {
-    name: 'Token',
-    tip: '例如：1|1bJbwlqBfnggmOMEZqXT5XusaIwqiZjCDs7r1Ob5',
-    width: 300,
-    path: 'token',
-    component: () => {
-      return h(CodeInput, {
-        value: token.value,
-        placeholder: '请填写图床生成的Token',
-        onUpdateValue: (val: string) => {
-          token.value = val
-        },
-        onChange: formValidation,
-      })
-    },
-  },
-  {
-    name: '存储策略',
-    component: () => {
-      return h('div', { class: 'flex gap-1' }, {
-        default: () => [
-          h(NSelect, {
-            value: activeStrategy.value,
-            onUpdateValue: (val: number) => {
-              activeStrategy.value = val
-              saveSetting()
-            },
-            options: settings.value.strategies,
-          }),
-          h(NButton, {
-            onClick: syncStrategies,
-          }, {
-            default: renderIcon('i-ic-round-refresh'),
-          }),
-        ],
-      })
-    },
-  },
-])
-
-function formValidation() {
-  setItem.value?.formValidation(() => {
-    settings.value.activeStrategy = null
-    settings.value.strategies = []
-    saveSetting()
-  })
-}
+// function formValidation() {
+//   setItem.value?.formValidation(() => {
+//     setting.value.activeStrategy = null
+//     setting.value.strategies = []
+//     saveSetting()
+//   })
+// }
 
 async function syncStrategies() {
   const loading = window.$message.loading('正在同步线上策略列表...')
@@ -96,30 +36,61 @@ async function syncStrategies() {
     window.$message.error('同步策略列表失败，请检查设置是否填写有误')
   loading.destroy()
 }
-
-async function saveSetting() {
-  programStore.setProgramDetail(id.value, { api: api.value, token: token.value })
-  if (api.value && token.value && settings.value.activeStrategy === null)
-    await syncStrategies()
-  programStore.setProgramDetail(id.value, { activeStrategy: activeStrategy.value })
-  window.$message.success('保存成功')
-}
-
-watch(() => route.params.id, () => {
-  nextTick(() => {
-    setItem.value?.resetValidation()
-  })
-})
-
-watchEffect(() => {
-  api.value = settings.value.api
-  token.value = settings.value.token
-  activeStrategy.value = settings.value.activeStrategy
-})
 </script>
 
 <template>
-  <div>
-    <SettingSection ref="setItemRef" class="pt0" :items="settingOptions" :rules />
-  </div>
+  <n-form ref="setFormRef" :rules="rules">
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          API 地址
+        </div>
+        <n-text class="text-xs op80">
+          http(s)://域名，不含尾随斜杠
+        </n-text>
+      </div>
+      <CodeInput v-model:value="setting.api" type="text" placeholder="请填写图床API地址" />
+    </n-card>
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          Token
+        </div>
+        <n-text class="text-xs op80">
+          例如：1|1bJbwlqBfnggmOMEZqXT5XusaIwqiZjCDs7r1Ob5
+        </n-text>
+      </div>
+      <CodeInput v-model:value="setting.token" type="text" placeholder="请填写图床生成的Token" />
+    </n-card>
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          存储策略
+        </div>
+      </div>
+      <div class="flex gap-1">
+        <n-select v-model:value="activeStrategy" :options="setting.strategies" />
+        <n-button @click="syncStrategies">
+          <div i-ic-round-refresh />
+        </n-button>
+      </div>
+    </n-card>
+  </n-form>
 </template>
+
+<style scoped>
+.set-item :deep(.n-card__content) {
+  --uno: flex flex-row items-center justify-between;
+}
+
+.set-item :deep(.n-select) {
+  --uno: w50;
+}
+
+.set-item :deep(.n-input) {
+  --uno: w75;
+}
+:deep(.n-form-item) > .n-form-item-feedback-wrapper {
+  --uno: text-xs;
+}
+</style>
--- src/components/Setting/ConfigS3.vue
+++ src/components/Setting/ConfigS3.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { type FormRules, NSwitch } from 'naive-ui'
+import type { FormRules } from 'naive-ui'
 import CodeInput from '~/components/Common/CodeInput.vue'
 import type { ProgramDetail } from '~/stores'
 import { useProgramStore } from '~/stores'
@@ -8,180 +8,142 @@ const route = useRoute('/Setting/[id]')
 const id = ref(Number.parseInt(route.params.id))
 const programStore = useProgramStore()
 
-const setting = computed(() => programStore.getProgram(id.value).detail as ProgramDetail['s3'])
+const setting = computed({
+  get: () => programStore.getProgram(id.value).detail as ProgramDetail['s3'],
+  set: val => programStore.setProgramDetail(id.value, val),
+})
 
-// S3 配置项
-const accessKeyId = ref(setting.value.accessKeyId)
-const secretAccessKey = ref(setting.value.secretAccessKey)
-const bucketName = ref(setting.value.bucketName)
-const pathPrefix = ref(setting.value.pathPrefix)
-const region = ref(setting.value.region)
-const endpoint = ref(setting.value.endpoint)
-const customDomain = ref(setting.value.customDomain)
-const forcePathStyle = ref(setting.value.forcePathStyle)
-const acl = ref(setting.value.acl)
-
-const setItem = useTemplateRef('setItemRef') // for validation
+const setForm = useTemplateRef('setFormRef')
 
 const rules: FormRules = {
-  // apiUrl: createFormRule(() => validateUrl(api.value)),
-  // token: createFormRule(() => validateLskyToken(token.value)),
-}
-
-const settingOptions = computed(() => [
-  {
-    name: 'accessKeyId',
-    width: 300,
-    tip: '访问ID',
-    component: () => {
-      return h(CodeInput, {
-        value: accessKeyId.value,
-        type: 'text',
-        onUpdateValue: (val: string) => {
-          accessKeyId.value = val
-          saveSetting()
-        },
-      })
-    },
-  },
-  {
-    name: 'secretAccessKey',
-    width: 300,
-    tip: '访问密钥',
-    component: () => {
-      return h(CodeInput, {
-        value: secretAccessKey.value,
-        type: 'text',
-        onUpdateValue: (val: string) => {
-          secretAccessKey.value = val
-          saveSetting()
-        },
-      })
-    },
-  },
-  {
-    name: 'bucketName',
-    width: 300,
-    tip: '存储桶名称',
-    component: () => {
-      return h(CodeInput, {
-        value: bucketName.value,
-        type: 'text',
-        onUpdateValue: (val: string) => {
-          bucketName.value = val
-          saveSetting()
-        },
-      })
-    },
-  },
-  {
-    name: 'pathPrefix',
-    width: 300,
-    tip: '上传路径前缀',
-    component: () => {
-      return h(CodeInput, {
-        value: pathPrefix.value,
-        type: 'text',
-        onUpdateValue: (val: string) => {
-          pathPrefix.value = val
-          saveSetting()
-        },
-      })
-    },
-  },
-  {
-    name: 'region',
-    width: 300,
-    tip: '区域',
-    component: () => {
-      return h(CodeInput, {
-        value: region.value,
-        type: 'text',
-        onUpdateValue: (val: string) => {
-          region.value = val
-          saveSetting()
-        },
-      })
-    },
-  },
-  {
-    name: 'endpoint',
-    width: 300,
-    tip: 'API端点',
-    component: () => {
-      return h(CodeInput, {
-        value: endpoint.value,
-        type: 'text',
-        onUpdateValue: (val: string) => {
-          endpoint.value = val
-          saveSetting()
-        },
-      })
-    },
-  },
-  {
-    name: 'customDomain',
-    width: 300,
-    tip: '自定义域名',
-    component: () => {
-      return h(CodeInput, {
-        value: customDomain.value,
-        type: 'text',
-        onUpdateValue: (val: string) => {
-          customDomain.value = val
-          saveSetting()
-        },
-      })
-    },
-  },
-  {
-    name: 'forcePathStyle',
-    tip: '存储桶名作为URL路径，仅支持 AWS S3',
-    component: () => {
-      return h(NSwitch, {
-        value: forcePathStyle.value,
-        round: false,
-        onUpdateValue: (val: boolean) => {
-          forcePathStyle.value = val
-          saveSetting()
-        },
-      })
-    },
-  },
-  {
-    name: 'ACL',
-    width: 300,
-    tip: '访问控制列表',
-    component: () => {
-      return h(CodeInput, {
-        value: acl.value,
-        type: 'text',
-        onUpdateValue: (val: string) => {
-          acl.value = val
-          saveSetting()
-        },
-      })
-    },
-  },
-])
-
-async function saveSetting() {
-  await programStore.setProgramDetail(id.value, {
-    accessKeyId: accessKeyId.value,
-    secretAccessKey: secretAccessKey.value,
-    bucketName: bucketName.value,
-    pathPrefix: pathPrefix.value,
-    region: region.value,
-    endpoint: endpoint.value,
-    customDomain: customDomain.value,
-    forcePathStyle: forcePathStyle.value,
-    acl: acl.value,
-  })
+//   accessKeyId: ,
+//   secretAccessKey: ,
+//   bucketName: ,
+//   pathPrefix: ,
+//   region: ,
+//   endpoint: ,
+//   customDomain: ,
+//   acl: ,
 }
 </script>
 
 <template>
-  <div>
-    <SettingSection ref="setItemRef" class="pt0" :items="settingOptions" :rules />
-  </div>
+  <n-form ref="setFormRef" :rules="rules">
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          accessKeyId
+        </div>
+        <n-text class="text-xs op80">
+          访问ID
+        </n-text>
+      </div>
+      <CodeInput v-model:value="setting.accessKeyId" type="text" />
+    </n-card>
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          secretAccessKey
+        </div>
+        <n-text class="text-xs op80">
+          访问密钥
+        </n-text>
+      </div>
+      <CodeInput v-model:value="setting.secretAccessKey" type="text" />
+    </n-card>
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          bucketName
+        </div>
+        <n-text class="text-xs op80">
+          存储桶名称
+        </n-text>
+      </div>
+      <CodeInput v-model:value="setting.bucketName" type="text" />
+    </n-card>
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          pathPrefix
+        </div>
+        <n-text class="text-xs op80">
+          上传路径前缀
+        </n-text>
+      </div>
+      <CodeInput v-model:value="setting.pathPrefix" type="text" />
+    </n-card>
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          region
+        </div>
+        <n-text class="text-xs op80">
+          区域
+        </n-text>
+      </div>
+      <CodeInput v-model:value="setting.region" type="text" />
+    </n-card>
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          endpoint
+        </div>
+        <n-text class="text-xs op80">
+          API端点
+        </n-text>
+      </div>
+      <CodeInput v-model:value="setting.endpoint" type="text" />
+    </n-card>
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          customDomain
+        </div>
+        <n-text class="text-xs op80">
+          自定义域名
+        </n-text>
+      </div>
+      <CodeInput v-model:value="setting.customDomain" type="text" />
+    </n-card>
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          forcePathStyle
+        </div>
+        <n-text class="text-xs op80">
+          存储桶名作为URL路径，仅支持 AWS S3
+        </n-text>
+      </div>
+      <n-switch v-model:value="setting.forcePathStyle" :round="false" />
+    </n-card>
+    <n-card class="set-item mb3 wh-full rounded-2">
+      <div class="flex flex-1 flex-col pr text-3.8 font-500 tracking-wider">
+        <div class="flex items-center">
+          ACL
+        </div>
+        <n-text class="text-xs op80">
+          访问控制列表
+        </n-text>
+      </div>
+      <CodeInput v-model:value="setting.acl" type="text" />
+    </n-card>
+  </n-form>
 </template>
+
+<style scoped>
+.set-item :deep(.n-card__content) {
+  --uno: flex flex-row items-center justify-between;
+}
+
+.set-item :deep(.n-select) {
+  --uno: w50;
+}
+
+.set-item :deep(.n-input) {
+  --uno: w75;
+}
+:deep(.n-form-item) > .n-form-item-feedback-wrapper {
+  --uno: text-xs;
+}
+</style>
