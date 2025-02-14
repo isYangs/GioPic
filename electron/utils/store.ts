f015eff222b808f359db6ad7ed45e9f6b003ceed
isYangs <isYangs@foxmail.com>
2025-02-14 13:29:31 +0800

    fix(store): enhance serialization

--- electron/utils/store.ts
+++ electron/utils/store.ts
@@ -1,15 +1,27 @@
 import { ipcMain } from 'electron'
 import Store from 'electron-store'
 
-const store = new Store()
+const store = new Store({
+  serialize: (data) => {
+    const parsed = typeof data === 'string' ? JSON.parse(data) : data
+    return JSON.stringify(parsed, null, 2)
+  },
+  deserialize: (data: string) => {
+    const parsed = JSON.parse(data)
+    return typeof parsed === 'string' ? JSON.parse(parsed) : parsed
+  },
+})
+
 const defaultStoreKey = '__giopic_app_store__'
+
 export function initStore() {
   ipcMain.handle('get-store', (_event, key) => {
     return store.get(key)
   })
 
   ipcMain.handle('set-store', (_event, key, value) => {
-    store.set(key, value)
+    const valueToStore = typeof value === 'string' ? JSON.parse(value) : value
+    store.set(key, valueToStore)
   })
 
   ipcMain.handle('delete-store', (_event, key) => {
@@ -19,7 +31,8 @@ export function initStore() {
 
 export function getStore(key: string, storeKey: string = defaultStoreKey) {
   try {
-    return JSON.parse(store.get(storeKey) as string)[key]
+    const data = store.get(storeKey)
+    return data?.[key]
   }
   catch (e) {
     console.error(e)
@@ -29,9 +42,9 @@ export function getStore(key: string, storeKey: string = defaultStoreKey) {
 
 export function setStore(key: string, value: any, storeKey: string = defaultStoreKey) {
   try {
-    const data = JSON.parse(store.get(storeKey) as string)
+    const data = store.get(storeKey) || {}
     data[key] = value
-    store.set(storeKey, JSON.stringify(data))
+    store.set(storeKey, data)
   }
   catch (e) {
     console.error(e)
--- src/components/Setting/ConfigLsky.vue
+++ src/components/Setting/ConfigLsky.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import type { FormRules } from 'naive-ui'
 import requestUtils from '~/api'
 import type { ProgramDetail } from '~/stores'
 import { useProgramStore } from '~/stores'
@@ -7,27 +6,11 @@ import { useProgramStore } from '~/stores'
 const route = useRoute('/Setting/[id]')
 const id = ref(Number.parseInt(route.params.id))
 const programStore = useProgramStore()
-const activeStrategy = ref<number | null>(null)
 
 const setting = computed({
   get: () => programStore.getProgram(id.value).detail as ProgramDetail['lsky'],
   set: val => programStore.setProgramDetail(id.value, val),
 })
-
-const setItem = useTemplateRef('setFormRef')
-
-const rules: FormRules = {
-  // apiUrl: createFormRule(() => validateUrl(api.value)),
-  // token: createFormRule(() => validateLskyToken(token.value)),
-}
-
-// function formValidation() {
-//   setItem.value?.formValidation(() => {
-//     setting.value.activeStrategy = null
-//     setting.value.strategies = []
-//     saveSetting()
-//   })
-// }
 
 async function syncStrategies() {
   const loading = window.$message.loading('正在同步线上策略列表...')
@@ -38,7 +21,7 @@ async function syncStrategies() {
 </script>
 
 <template>
-  <n-form ref="setFormRef" :rules="rules">
+  <n-form>
     <setting-item title="API 地址" desc="http(s)://域名，不含尾随斜杠">
       <code-input v-model:value="setting.api" type="text" placeholder="请填写图床API地址" />
     </setting-item>
@@ -49,7 +32,10 @@ async function syncStrategies() {
 
     <setting-item title="存储策略">
       <div class="flex gap-1">
-        <n-select v-model:value="activeStrategy" :options="setting.strategies" />
+        <n-select
+          v-model:value="setting.activeStrategy"
+          :options="setting.strategies"
+        />
         <n-button @click="syncStrategies">
           <div i-ic-round-refresh />
         </n-button>
